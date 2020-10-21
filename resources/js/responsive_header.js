0924f0d6cc014ff87cc02b15be586b1e675de685
CommandMaker <63251660+CommandMaker@users.noreply.github.com>
2020-10-21 16:28:28 +0200

    Assombrissement lors de l'ouverture du menu sur mobile, ajout d'icones et fin du menu mobile

--- resources/js/responsive_header.js
+++ resources/js/responsive_header.js
@@ -3,8 +3,10 @@ jQuery(($) => {
         $('body').toggleClass('is-open');
         if ($('body').hasClass('is-open')) {
             $('.toggle-menu').css('transform', 'translateX(0)');
+            $('.header-texture').css('opacity', '0.6');
         } else {
             $('.toggle-menu').css('transform', 'translateX(200px)');
+            $('.header-texture').css('opacity', '0');
         }
         e.preventDefault();
     });
